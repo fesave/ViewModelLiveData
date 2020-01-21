ba71a51e150a9085c6c9cd7b7f48cffe059aaa7a
fesave <fernando.sanz.velasco@gmail.com>
2020-01-21 12:50:17 +0100

    Finished S02-CL17

--- app/src/main/java/com/machucapps/viewmodellivedata/ui/LiveDataActivity.kt
+++ app/src/main/java/com/machucapps/viewmodellivedata/ui/LiveDataActivity.kt
@@ -2,13 +2,17 @@ package com.machucapps.viewmodellivedata.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.machucapps.viewmodellivedata.R
 import com.machucapps.viewmodellivedata.utils.User
+import com.machucapps.viewmodellivedata.viewmodel.LiveDataViewModel
 import kotlinx.android.synthetic.main.activity_live_data.*
 
 class LiveDataActivity : AppCompatActivity() {
 
     var number: Int = 0
+    lateinit var viewModel: LiveDataViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +21,41 @@ class LiveDataActivity : AppCompatActivity() {
     }
 
     private fun setUpView() {
+        viewModel = ViewModelProviders.of(this).get(LiveDataViewModel::class.java)
+
         btSave.setOnClickListener {
             if (number == 0) {
                 val user = User()
                 user.name = "Juan"
                 user.age = "30"
+                viewModel.addUser(user)
+
             }
             if (number == 1) {
                 val user = User()
                 user.name = "Sofía"
                 user.age = "45"
+                viewModel.addUser(user)
+
             }
             if (number == 2) {
                 val user = User()
                 user.name = "Felipe"
                 user.age = "65"
+                viewModel.addUser(user)
+
             }
+            number++
         }
+
+        val listObserver = Observer<List<User>> { userList ->
+            var text = ""
+            for (user in userList) {
+                text += "${user.name} - ${user.age} \n"
+            }
+            tvLiveData.text = text
+        }
+
+        viewModel.getUserList().observe(this@LiveDataActivity, listObserver)
     }
 }
