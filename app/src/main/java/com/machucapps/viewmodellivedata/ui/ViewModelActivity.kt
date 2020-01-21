97cd2a5dba449c08af8f67dd59b90905a87c1c40
fesave <fernando.sanz.velasco@gmail.com>
2020-01-21 10:23:58 +0100

    Finished S02-CL08

--- app/src/main/java/com/machucapps/viewmodellivedata/ui/ViewModelActivity.kt
+++ app/src/main/java/com/machucapps/viewmodellivedata/ui/ViewModelActivity.kt
@@ -1,13 +1,26 @@
 package com.machucapps.viewmodellivedata.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.machucapps.viewmodellivedata.R
+import com.machucapps.viewmodellivedata.utils.Sumar
+import kotlinx.android.synthetic.main.activity_view_model.*
 
 class ViewModelActivity : AppCompatActivity() {
+
+    private var resultado: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_model)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        tvSumar.text = "$resultado"
+        btSumar.setOnClickListener {
+            resultado = Sumar.sumar(resultado)
+            tvSumar.text = "$resultado"
+        }
     }
 }
---
+++ app/src/main/java/com/machucapps/viewmodellivedata/utils/Sumar.kt
@@ -0,0 +1,6 @@
+package com.machucapps.viewmodellivedata.utils
+
+object Sumar {
+
+    fun sumar(number: Int) = number + 1
+}
