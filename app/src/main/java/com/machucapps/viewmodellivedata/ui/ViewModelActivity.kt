9e3160b10fe54199a59513f4bb2ff0fae72197cb
fesave <fernando.sanz.velasco@gmail.com>
2020-01-21 10:33:18 +0100

    Finished S02-CL09

--- app/src/main/java/com/machucapps/viewmodellivedata/ui/ViewModelActivity.kt
+++ app/src/main/java/com/machucapps/viewmodellivedata/ui/ViewModelActivity.kt
@@ -1,6 +1,7 @@
 package com.machucapps.viewmodellivedata.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.machucapps.viewmodellivedata.R
 import com.machucapps.viewmodellivedata.utils.Sumar
@@ -9,9 +10,11 @@ import kotlinx.android.synthetic.main.activity_view_model.*
 class ViewModelActivity : AppCompatActivity() {
 
     private var resultado: Int = 0
+    private var TAG = ViewModelActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate()")
         setContentView(R.layout.activity_view_model)
         setUpView()
     }
@@ -22,5 +25,34 @@ class ViewModelActivity : AppCompatActivity() {
             resultado = Sumar.sumar(resultado)
             tvSumar.text = "$resultado"
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume()")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause()")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop()")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy()")
+
     }
 }
