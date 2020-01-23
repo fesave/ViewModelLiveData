553a3ad81b55de68ed7925f1bcded6b621104dea
fesave <fernando.sanz.velasco@gmail.com>
2020-01-23 09:29:30 +0100

    Finished S04-CL19

--- app/src/main/java/com/machucapps/viewmodellivedata/ui/DataBindingActivity.kt
+++ app/src/main/java/com/machucapps/viewmodellivedata/ui/DataBindingActivity.kt
@@ -2,7 +2,9 @@ package com.machucapps.viewmodellivedata.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.machucapps.viewmodellivedata.R
+import com.machucapps.viewmodellivedata.databinding.ActivityDataBindingBinding
 import com.machucapps.viewmodellivedata.utils.User
 
 class DataBindingActivity : AppCompatActivity() {
@@ -11,6 +13,13 @@ class DataBindingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_binding)
+        //setContentView(R.layout.activity_data_binding)
+
+        val binding: ActivityDataBindingBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
+
+        user = User("Pedro", "45")
+
+        binding.user = this.user
     }
 }
